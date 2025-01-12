e1e57de653bec50e8be6889819d2c25b376b240f
Filiph W <filiph.wallsten@gmail.com>
2025-01-12 09:13:35 +0100

    asd

--- cards-against/src/components/playerHand.tsx
+++ cards-against/src/components/playerHand.tsx
@@ -1,4 +1,27 @@
+import usePlayerLobbyStore from "@/store/playerLobbyStore";
+import { useEffect } from "react";
+import pb
+ from "@/lib/pocketbase";
 const PlayerHand = () => {
+  const { playerId } = usePlayerLobbyStore();
+
+  useEffect(() => {
+    const fetchPlayerHandsCard = async () => { 
+      try {
+            const resultList = await pb.collection('player_cards').getList(1, 50, {
+              filter: `player_id="${playerId}"`,
+              expand: "card_id", // Include related player data
+        });
+        console.log(resultList)
+        return resultList;
+         } catch(e) {
+           console.log(e)
+         }
+      }
+
+      fetchPlayerHandsCard()
+  },[])
+  
   return (
     <div className="flex flex-row gap-2">
       <div className="w-[200px] p-4 h-[240px] border-2 border-slate-900 rounded hover:scale-110">
